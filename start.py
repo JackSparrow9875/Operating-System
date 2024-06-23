aeba8cd2ca1137a406110e5099cb37c96ba23c5f
JackSparrow9875 <rishiveer07@outlook.com>
2024-06-24 02:10:19 +0530

    major modifications made and the server is now up and running!

--- start.py
+++ start.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
-__name__ = '__main__'
 app = Flask(__name__)
 
 print('Hi')
+
+@app.route('/')
+def startup():
+    return render_template('startup.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
